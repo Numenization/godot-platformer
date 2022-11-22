ced10a4d6cee5dde499e1d2ab4704bc3bf3c24b5
numenization <nicholas.spencer5@gmail.com>
2022-11-21 21:35:31 -0500

    added camera zones and expanded map

--- assets/player/Player.gd
+++ assets/player/Player.gd
@@ -1,6 +1,8 @@
 class_name Player
 extends KinematicBody2D
 
+export (NodePath) var camera_path
+
 export var gravity = 4
 export var walk_speed = 60
 export var sprint_speed = 90
@@ -14,9 +16,11 @@ export var dash_length = 0.5
 
 var velocity = Vector2.ZERO
 var move_speed = 60
+var camera_zone = null
 
 onready var animations : AnimatedSprite = $AnimatedSprite
 onready var states = $StateManager
+onready var camera : Camera2D = get_node(camera_path)
 
 func _ready() -> void:
 	$"StateManager/DashTimer".wait_time = dash_length
@@ -27,9 +31,19 @@ func _unhandled_input(event: InputEvent) -> void:
 	
 func _physics_process(delta: float) -> void:
 	states.physics_process(delta)
+	check_camera()
 	
 func _process(delta: float) -> void:
 	states.process(delta)
+	
+func check_camera() -> void:
+	var physics = get_world_2d().get_direct_space_state()
+	var points = physics.intersect_point(position, 1, [], 2147483647, false, true)
+	for point in points:
+		if point['collider'] != camera_zone:
+			camera_zone = point['collider']
+			camera.position = camera_zone.position
+			print("entering camera zone: %s (%s,%s)" % [camera_zone, camera_zone.position.x, camera_zone.position.y])
 	
 func get_movement_input() -> int:
 	if Input.is_action_pressed("move_left"):
